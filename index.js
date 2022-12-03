59e8f2b691af55cf7024464fe45f75b5748e0289
rsheridan613 <rsheridan613@gmail.com>
2022-12-02 17:40:28 -0700

    added mysql, dotenv. require in index

---
+++ index.js
@@ -0,0 +1,12 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "employees_db",
+  },
+  console.log(`Connected to the employees_db database.`)
+);

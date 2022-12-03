59a3e69934cd2d5423cfc898919db008173e108e
rsheridan613 <rsheridan613@gmail.com>
2022-12-03 15:17:05 -0700

    added seeds for department, role

--- db/schema.sql
+++ db/schema.sql
@@ -4,24 +4,24 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE departments(
-id INT PRIMARY KEY AUTO_INCREMENT,
+id INT NOT NULL PRIMARY KEY,
 name VARCHAR(30)
 );
 
 CREATE TABLE role(
-id INT PRIMARY KEY,
-title VARCHAR(30),
-salary DECIMAL,
-department_id INT,
+id INT NOT NULL PRIMARY KEY,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL NOT NULL,
+department_id INT NOT NULL,
 FOREIGN KEY (department_id)
 REFERENCES departments(id)
 );
 
 CREATE TABLE employee(
-id INT PRIMARY KEY,
-first_name VARCHAR(30),
-last_name VARCHAR(30),
-role_id INT,
+id INT PRIMARY KEY NOT NULL,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT NOT NULL,
 manager_id INT,
 FOREIGN KEY (role_id)
 REFERENCES role(id),
---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+USE employees_db;
+
+INSERT INTO department(id, name)
+VALUES
+(001, "Executive"),
+(002, "Finance"),
+(003, "Human Recources"),
+(004, "Research and Development");
+
+INSERT INTO role(id, title, salary, department_id)
+VALUES
+(001, "Owner", 200,000, 001),
+(002, "Finance Manager", 175.000, 002),
+(003, "Finance Expert", 125.000, 002),
+(004, "HR Manager", 165.000, 003),
+(005, "HR Expert", 100,000, 003),
+(006, "R+D Manager", 180.000, 004),
+(006, "R+D Expert", 155.000, 004),

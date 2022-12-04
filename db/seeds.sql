328c73b2d91d68521d0e7d41b81ef3cf45379310
rsheridan613 <rsheridan613@gmail.com>
2022-12-04 09:34:58 -0700

    didn't save seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -15,7 +15,7 @@ VALUES
 ("R+D Manager", 180.000, 004),
 ("R+D Expert", 155.000, 004);
 
-INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES
 (),
 (),

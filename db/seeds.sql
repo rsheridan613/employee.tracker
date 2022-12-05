b6bcb3d3361808845377eafbd91724d2db7db78f
rsheridan613 <rsheridan613@gmail.com>
2022-12-04 21:14:03 -0700

    added more to emplpoyee seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,25 +1,26 @@
 INSERT INTO department(name)
 VALUES
 ("Executive"),
-
 ("Finance"),
 ("Human Recources"),
 ("Research and Development");
 
-INSERT INTO roles(title, salary, department_id)
+INSERT INTO role(title, salary, department_id)
 VALUES
-("Owner", 200.000, 001),
-("Finance Manager", 175.000, 002),
-("Finance Expert", 125.000, 002),
-("HR Manager", 165.000, 003),
-("HR Expert", 100.000, 003),
-("R+D Manager", 180.000, 004),
-("R+D Expert", 155.000, 004);
+("Owner", 200000, 001),
+("Finance Manager", 175000, 002),
+("Finance Expert", 125000, 002),
+("HR Manager", 165000, 003),
+("HR Expert", 100000, 003),
+("R+D Manager", 180000, 004),
+("R+D Expert", 155000, 004);
 
-INSERT INTO employees(first_name, last_name, role_id, manager_id)
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES
 ("Master", "Yoda", 1, NULL),
 ("Qui-Gon", "Jinn", 2, 1),
 ("Obi-Wan", "Kenobi", 3, 2),
 ("Anakin","Skywalker", 4, 1),
-("Ahsoka", "Tano", 5, 4);
+("Ahsoka", "Tano", 5, 4),
+("Luminara", "Unduli", 6, 1),
+("Barriss", "Offee", 7, 4);
